d64d3d5335f6c81842313936a2f326f48334cae1
GSamuel <gideon_hoog@hotmail.com>
2014-08-04 11:25:57 +0200

    Character

---
+++ src/character/Card.java
@@ -0,0 +1,6 @@
+package character;
+
+public enum Card
+{
+	HUMAN, WEREWOLF, CLAIRVOYANT, WITCH, SLUT, HUNTER, CUPID, HEALER,
+}
--- src/character/Character.java
+++ src/character/Character.java
@@ -5,13 +5,19 @@ public class Character
 	private String name;
 	private Gender gender;
 	private Age age;
-	//private Race race;
+	
+	private Card card;
+	
+	private Race race;
 	
 	public Character(String name, Gender gender, Age age)
 	{
 		this.name = name;
 		this.gender = gender;
 		this.age = age;
+		
+		this.card = Card.HUMAN;
+		this.race = Race.HUMAN;
 	}
 	
 	public String getName()
@@ -27,5 +33,25 @@ public class Character
 	public Age getAge()
 	{
 		return age;
+	}
+	
+	public Card getCard()
+	{
+		return card;
+	}
+	
+	public void setCard(Card card)
+	{
+		this.card = card;
+	}
+	
+	public Race getRace()
+	{
+		return race;
+	}
+	
+	public void setRace(Race race)
+	{
+		this.race = race;
 	}
 }
